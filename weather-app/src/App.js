c59ff85e9346ffa8d2df7def5a6fe58e854255b5
chiranjibi.lawati@infodeveloper.com.np <chiranjibilawati08@gmail.com>
2020-08-03 18:18:55 +0545

    certain changes made on app file

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -7,8 +7,22 @@ const api = {
 
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January","Feburary","March","April","May","June",
+                  "July","August","September","October","November","December"];
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
-    <div className="App">
+    <div className="app">
       <main>
         <div className="search-box">
           <input 
@@ -16,6 +30,19 @@ function App() {
             className="search-bar"
             placeholder="Search..."
           />
+        </div>
+        <div className="location-box">
+          <div className="location">Kathmandu City, Nepal
+            <div className="date">
+                {dateBuilder(new Date())}
+            </div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              15*C
+            </div>
+            <div className="weather">Sunny</div>
+          </div>
         </div>
       </main>
     </div>
